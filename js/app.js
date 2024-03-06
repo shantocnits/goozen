e8b848c044ef5e5c0904f3db753c828cd1bd9d5d
rahatcnits <rahatcnits@gmail.com>
2024-03-06 14:57:06 +0600

    slider next prev button err fix

--- js/app.js
+++ js/app.js
@@ -1,51 +1,48 @@
 // Hero Slider
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      loop: true,
-  });
-  // Hero Slider End
-
-
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+// Hero Slider End
 
 // Feature Categories Start
-  var swiper = new Swiper(".slide-container", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    sliderPerGroup: 4,
-    loop: true,
-    centerSlide: "true",
-    fade: "true",
-    grabCursor: "true",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  sliderPerGroup: 5,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      520: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1000: {
-        slidesPerView: 4,
-      },
+    768: {
+      slidesPerView: 3,
     },
-  });
-  // Feature Categories End
-
+    1000: {
+      slidesPerView: 5,
+    },
+  },
+});
+// Feature Categories End
