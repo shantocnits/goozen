beacdaa85a1fba63325dc6c8161d39faabc7f535
shantocnits <shanto.cnits@gmail.com>
2024-03-09 03:08:12 +0600

    trending products

---
+++ js/logo-slider.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".footer-slider", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+});
