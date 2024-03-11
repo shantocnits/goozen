edd46004c489e5e0f2daa9df1a24fbc5d68b2658
shantocnits <shanto.cnits@gmail.com>
2024-03-11 09:22:07 +0600

    shop repo

---
+++ js/top$selling-slider.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".footer-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
