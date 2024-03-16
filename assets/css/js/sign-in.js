946792129414732e6f70b9a650429b05af4234b7
shantocnits <shanto.cnits@gmail.com>
2024-03-17 00:20:29 +0600

    sign in repo

---
+++ assets/css/js/sign-in.js
@@ -0,0 +1,21 @@
+// const formWrapper = document.querySelector(".form_wrapper");
+// const signInBtn = document.querySelector(".signInBtn");
+// const forms = document.querySelector(".forms"),
+// links = document.querySelectorAll(".link");     
+
+// signInBtn.addEventListener("click", () => {
+//     formWrapper.classList.add("active");
+// });
+
+// formWrapper.addEventListener("click", (e) => {
+//     if(e.target == formWrapper) {
+//         formWrapper.classList.remove("active");
+//     }
+// });
+
+// links.forEach((link) => {
+//     link.addEventListener("click", (e) => {
+//     e.preventDefault(); //preventing form submit
+//     forms.classList.toggle("show-signup");
+//     });
+// });
