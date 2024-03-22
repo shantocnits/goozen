212ac90d5b3d44abe848b0d8d5542fa6a45420e1
shantocnits <shanto.cnits@gmail.com>
2024-03-22 16:48:38 +0600

    categories slider

---
+++ assets/js/categories-slider.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.categories-slider', {
+  slidesPerView: 5,
+  spaceBetween: 17,
+  slidesPerGroup: 1,
+      direction: 'vertical',
+      effect: 'slide',
+      loop: true,
+      loopFillGroupWithBlank: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+      },
+      
+    });
+
+    function getDirection() {
+      var windowWidth = window.innerWidth;
+      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+      return direction;
+    }
+    
