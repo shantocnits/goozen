36bf325a8eb0f01ab1addd82e607eeff7164cd47
shantocnits <shanto.cnits@gmail.com>
2024-03-23 10:14:05 +0600

    explore slider

---
+++ assets/js/blog-explore.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".review", {
+    slidesPerView: 1,
+    spaceBetween: 110,
+    sliderPerGroup: 1,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1000: {
+        slidesPerView: 1,
+      },
+    },
+  });
