ade9930939a57456d146345a136bde1e5e99b02d
shantocnits <shanto.cnits@gmail.com>
2024-04-17 01:06:57 +0600

    deals slider

--- assets/js/deal-slider.js
+++ assets/js/deal-slider.js
@@ -1,21 +1,20 @@
-var swiper = new Swiper('.deal-slider', {
-    slidesPerView: 2,
-    spaceBetween: 15,
-    slidesPerGroup: 1,
-        direction: 'vertical',
-        effect: 'slide',
-        loop: true,
-        loopFillGroupWithBlank: true,
-        navigation: {
-            nextEl: ".deal-next",
-            prevEl: ".deal-prev",
-          },
-        
-      });
-  
-      function getDirection() {
-        var windowWidth = window.innerWidth;
-        var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-  
-        return direction;
-      }
+var swiper = new Swiper(".myslide2", {
+  spaceBetween: 20,
+
+  direction: 'vertical',
+ loop: "true",
+  slidesPerView: 2,
+  freeMode: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper2 = new Swiper(".myslide", {
+
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
