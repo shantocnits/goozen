98a00e59d1c69fbdf92b202a00a5ad1f8329168b
shantocnits <shanto.cnits@gmail.com>
2024-04-24 23:41:31 +0600

    payment

--- assets/js/top-rated-slider.js
+++ assets/js/top-rated-slider.js
@@ -6,10 +6,29 @@ var swiper = new Swiper(".topslide2", {
   freeMode: true,
   watchSlidesProgress: true,
   navigation: {
-    nextEl: ".swiper-button-next",
+    nextEl: ".top-rated-next",
   },
 });
 var swiper2 = new Swiper(".topslide", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+// toip selling slider
+var swiper = new Swiper(".sellingslide2", {
+  spaceBetween: 20,
+  direction: 'vertical',
+  loop: "true",
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".top-selling-next",
+  },
+});
+var swiper2 = new Swiper(".sellingslide", {
   spaceBetween: 10,
   thumbs: {
     swiper: swiper,
